6912b97ceeabad982168dbed484513f114a607f4
yhjang <yhjang@eduwill.net>
2017-04-17 08:56:23 +0900

    검색기능 추가함

--- src/providers/github-users.ts
+++ src/providers/github-users.ts
@@ -30,4 +30,9 @@ export class GithubUsers {
       .map(res => <User>(res.json()));
   }
 
+  searchUsers(searchParam: string): Observable<User[]>{
+    return this.http.get(`${this.githubApiUrl}/search/users?q=${searchParam}`)
+    .map( res => <User[]>(res.json().items));
+  }
+
 }
